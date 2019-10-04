8c806efdfeef94afa9b2fa314370ee0b4182f6c5
Agustín Aliaga <agustin.aliaga@lavu.com>
2019-10-03 21:27:31 -0300

    WIP: Add new flyer

--- front-end/src/modules/map-screen/MapScreen.jsx
+++ front-end/src/modules/map-screen/MapScreen.jsx
@@ -1,35 +1,49 @@
 import React from 'react';
 import { shape, number } from 'prop-types';
 import GoogleMapReact from 'google-map-react';
+import { geolocated } from 'react-geolocated';
 import { MapWrapper } from './components/styled';
+import UserActions from '../common/UserActions';
 
-const SimpleMap = ({ center, zoom }) => (
-  // Important! Always set the container height explicitly
-  <MapWrapper>
-    <GoogleMapReact
-      bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
-      defaultCenter={center}
-      defaultZoom={zoom}
-    >
-      {/* <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" /> */}
-    </GoogleMapReact>
-  </MapWrapper>
-);
+const SimpleMap = ({ zoom, coords }) => {
+  const mapCoords = coords || {
+    latitude: -31.409690,
+    longitude: -64.188881,
+  };
+
+  return (
+    // Important! Always set the container height explicitly
+    <MapWrapper>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
+        center={{
+          lat: mapCoords.latitude,
+          lng: mapCoords.longitude,
+        }}
+        defaultZoom={zoom}
+      >
+        {/* <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" /> */}
+      </GoogleMapReact>
+      <UserActions />
+    </MapWrapper>
+  );
+};
 
 SimpleMap.propTypes = {
-  center: shape({
-    lat: number,
-    lng: number,
+  coords: shape({
+    latitude: number,
+    longitude: number,
   }),
   zoom: number,
 };
 
 SimpleMap.defaultProps = {
-  center: {
-    lat: 59.95,
-    lng: 30.33,
-  },
-  zoom: 11,
+  zoom: 15,
 };
 
-export default SimpleMap;
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+})(SimpleMap);
